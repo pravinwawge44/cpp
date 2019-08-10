778bf8a07109c8f03cd6a005611c4a4f5ca1e662
pravinwawge44 <53791410+pravinwawge44@users.noreply.github.com>
2019-08-10 21:53:29 +0530

    Update tripprogram.c

--- tripprogram.c
+++ tripprogram.c
@@ -1,80 +1,47 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
-int main(void)
+int students[1005];
+
+int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-
-	const char digit[][16] = {
-		"-|| ||-", // 0
-		" |   | ", // 1
-		"-| -| -", // 2
-		"-| - |-", // 3
-		" ||- | ", // 4
-		"- |- |-", // 5
-		"- |-||-", // 6
-		"-|   | ", // 7
-		"-||-||-", // 8
-		"-||- |-" // 9
-	};
-
-	int size = 0;
-	string number;
-
-	while (cin >> size and size)
-	{
-		cin >> number;
-
-		int height = (size << 1) + 3;
-
-		for (int i = 0; i < height; ++i)
-		{
-			for (int j = 0; j < number.length(); ++j)
-			{
-				int d = number[j] - '0';
-				
-
-				if (i > 0 and i < size + 1) 
-				{
-					cout << digit[d][2];
-					
-					for (int k = 0; k < size; ++k) 
-						cout << " ";
-					
-					cout << digit[d][1];
-				}
-
-				else if (i % (size + 1) == 0) 
-				{
-					cout << " ";
-					
-					for (int k = 0; k < size; ++k) 
-						cout << digit[d][i / (size + 1) * 3];
-
-					cout << " ";
-				}
-
-				else if (i > (size + 1) and i < height - 1) 
-				{
-					cout << digit[d][4];
-					
-					for (int k = 0; k < size; ++k) 
-						cout << " ";
-					
-					cout << digit[d][5];
-				}
-				// Separation between digits.
-				if (j != number.length() - 1)
-					cout << " ";
-			}
-
-			cout << "\n";
-		}
-		
-		cout << "\n";
-	}
-
-	return 0;
+    int N;
+    
+    while (cin >> N, N)
+    {
+        int sum = 0;
+        for (int i = 0; i < N; ++i)
+        {
+            int dollar, cent;
+            char temp;
+            cin >> dollar >> temp >> cent;
+            students[i] = dollar * 100 + cent;
+            sum += students[i];
+        }
+        
+        int lowAverage = sum / N;
+        int highAverage = lowAverage + ((sum % N) ? 1 : 0);
+        int sumAbove = 0;
+        
+        for (int i = 0; i < N; ++i)
+        {
+            if (students[i] > highAverage)
+                sumAbove += students[i] - highAverage;
+        }
+        
+        int sumBelow = 0;
+        for (int i = 0; i < N; ++i)
+        {
+            if (students[i] < lowAverage)
+                sumBelow += lowAverage - students[i];
+        }
+        
+        int usedSum = max(sumAbove, sumBelow);
+        
+        
+        cout << '$' << (usedSum / 100) << '.' << setw(2) << setfill('0') << (usedSum % 100) << '\n';
+    }
 }
